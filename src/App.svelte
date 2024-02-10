722ee9407aa14c074f750bc8526e492c7e163c1c
DESKTOP-GVERLB0\Zoe <1526566980@qq.com>
2024-02-10 22:05:06 +0800

    [add] 新增账单模态框

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-    import { Button, Modal, Label, Input, Checkbox } from "flowbite-svelte";
+    import { Button, Modal, Label, Input, Select } from "flowbite-svelte";
     // 打开模态框
     let formModal = false;
+    let countries = [{ value: "traffic", name: "交通" }];
+    const form = {
+        date: "",
+        type: "",
+        amount: 0,
+        totalAmount: 0,
+    };
+
+    /** 提交表单 */
+    const submit = () => {
+        console.log(form);
+    };
 </script>
 
 <div class="p-3.5">
@@ -14,40 +26,37 @@
 <Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
     <form class="flex flex-col space-y-6" action="#">
         <Label class="space-y-2">
-            <span>Email</span>
+            <span>日期</span>
             <Input
-                type="email"
-                name="email"
-                placeholder="name@company.com"
+                bind:value={form.date}
+                id="date"
+                name="date"
                 required
+                type="date"
             />
         </Label>
-        <Label class="space-y-2">
-            <span>Your password</span>
-            <Input
-                type="password"
-                name="password"
-                placeholder="•••••"
-                required
-            />
-        </Label>
-        <div class="flex items-start">
-            <Checkbox>Remember me</Checkbox>
-            <a
-                href="/"
-                class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500"
-            >
-                Lost password?
-            </a>
+        <div class="flex justify-between">
+            <Label class="space-y-2 w-2/5">
+                <span>类别</span>
+                <Select bind:value={form.type} items={countries} />
+            </Label>
+            <Label class="space-y-2 w-2/5">
+                <span>金额</span>
+                <Input
+                    bind:value={form.amount}
+                    id="amount"
+                    name="amount"
+                    required
+                    type="number"
+                />
+            </Label>
         </div>
-        <Button type="submit" class="w-full1">Login to your account</Button>
-        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered? <a
-                href="/"
-                class="text-primary-700 hover:underline dark:text-primary-500"
-            >
-                Create account
-            </a>
+        <div class="flex justify-between">
+            <div>总金额（元）</div>
+            <div class="font-semibold">{form.totalAmount}</div>
         </div>
+        <Button type="submit" class="w-full1" on:click={() => submit()}
+            >提交</Button
+        >
     </form>
 </Modal>
